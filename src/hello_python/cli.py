dd0190baa913bc4735ae8387f86b53212c9e7871
Siak Hooi <siakhooi@gmail.com>
2025-04-20 06:56:57 +0000

    touch up

--- src/hello_python/cli.py
+++ src/hello_python/cli.py
@@ -4,8 +4,7 @@ from hello_python.util import get_arguments
 
 
 def run():
-    print("Hello, Python!")
-    print("This is a CLI application.")
+    print("Hello, Python CLI!")
     print("argument list", sys.argv)
     argv = get_arguments(sys.argv)
     print("argument list", argv)
